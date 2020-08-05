22ca0468c288b63cb3405ff3f95ad0b9401da711
AAnnan <62538347+AAnnan@users.noreply.github.com>
2020-08-05 20:09:42 +0200

    Add files via upload

--- 01b_Demux_Guppy_Refine.sh
+++ 01b_Demux_Guppy_Refine.sh
@@ -40,8 +40,8 @@ cp ${GUPPY_DIR}/../data/barcoding/* ./rerio/basecall_models/barcoding/
 
 ### Run Guppy Basecaller (to refine demultiplexing)
 
-${GUPPY_DIR}/guppy_basecaller --input_path ./demultiplexed_fast5s_${expName}/${barcode[i]} \
-	--save_path ./guppyBC/${barcode[i]} \
+${GUPPY_DIR}/guppy_basecaller --input_path ./demultiplexed_fast5s_${expName}/${barcodesOfInterest[i]} \
+	--save_path ./guppyBC/${barcodesOfInterest[i]} \
 	--data_path ./rerio/basecall_models/ \
     --config res_dna_r941_min_modbases-all-context_v001.cfg \
 	--records_per_fastq 40000 --recursive \
@@ -50,19 +50,19 @@ ${GUPPY_DIR}/guppy_basecaller --input_path ./demultiplexed_fast5s_${expName}/${b
 	--device auto
 
 #Move fast5s from children folders to the parent
-find ./guppyBC/${barcode[i]}/workspace/ -type f -name "*.fast5" | xargs mv -t ./guppyBC/${barcode[i]}/workspace/
+find ./guppyBC/${barcodesOfInterest[i]}/workspace/ -type f -name "*.fast5" | xargs mv -t ./guppyBC/${barcodesOfInterest[i]}/workspace/
 #Create a txt file containing the absolute filename of all barcoded fast5s
-awk -v barcode="${barcode[i]}" '$21==barcode {print "./guppyBC/"barcode"/workspace/"$2".fast5"}' ./guppyBC/${barcode[i]}/sequencing_summary.txt > list_ids_${barcode[i]}.txt
+awk -v barcode="${barcodesOfInterest[i]}" '$21==barcode {print "./guppyBC/"barcode"/workspace/"$2".fast5"}' ./guppyBC/${barcodesOfInterest[i]}/sequencing_summary.txt > list_ids_${barcodesOfInterest[i]}.txt
 #Move the fast5s contained in the list to their final directory
-mkdir -p ./final_fast5s_${expName}/${barcode[i]}
-xargs mv -t ./final_fast5s_${expName}/${barcode[i]} < list_ids_${barcode[i]}.txt
+mkdir -p ./final_fast5s_${expName}/${barcodesOfInterest[i]}
+xargs mv -t ./final_fast5s_${expName}/${barcodesOfInterest[i]} < list_ids_${barcodesOfInterest[i]}.txt
 
 ### Perform single to multi fast5 conversion (for storage, as to not have millions of small files)
 
-single_to_multi_fast5 -i ./final_fast5s_${expName}/${barcode[i]}/ \
-    -s ${work_DIR}/final_multifast5s_${expName}/${barcode[i]}/ \
+single_to_multi_fast5 -i ./final_fast5s_${expName}/${barcodesOfInterest[i]}/ \
+    -s ${work_DIR}/final_multifast5s_${expName}/${barcodesOfInterest[i]}/ \
     --threads 30 \
-    --filename_base ${expName}_${barcode[i]} \
+    --filename_base ${expName}_${barcodesOfInterest[i]} \
     --batch_size 20000 
 
 conda deactivate
