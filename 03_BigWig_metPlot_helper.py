403cead63885322406528d807b881e10195b0a6c
AAnnan <ahrmad.annan@students.unibe.ch>
2020-10-02 17:36:46 +0200

    build plot on previously retrieved scroe list

--- 03_BigWig_metPlot_helper.py
+++ 03_BigWig_metPlot_helper.py
@@ -22,6 +22,9 @@ lib = sys.argv[1] #${barcodesOfInterest[${i}]}
 k = sys.argv[2]
 motifs = ['HCG','GCH','GCG']
 
+#Length of CE chromosomes in order (chrI chrII chrIII chrIV chrM chrV chrX)
+chrm_lens = [0,15072434,15279421,13783801,17493829,13794,20924180,17718942]
+
 ### Functions
 def get_score_list_per_motif(per_read_db_file,strand,motif):
 	"""
@@ -55,25 +58,6 @@ def get_score_list_per_motif(per_read_db_file,strand,motif):
 
 	return score_list
 
-def get_scores(per_read_db_file):
-	"""
-	Input:  per-read db location (str)
-	Ouput:  List with scores only, unexp
-	"""
-	#Connect to the DB
-	conn = sqlite3.connect(per_read_db_file)
-	c = conn.cursor()
-
-	#Query the database for scores
-	c.execute(f"SELECT score FROM data")
-	score_l = c.fetchall()
-
-	#Close the connection
-	conn.close()
-
-	return np.exp(score_l, dtype=np.float64)
-
-
 def save_methyl_prob_plot(score_list,motif,lib,k):
 	"""
 	Input:  score_list list or numpy array containing unlogged scores (0-1)
@@ -84,74 +68,77 @@ def save_methyl_prob_plot(score_list,motif,lib,k):
 	"""
 
 	score_count = np.array(np.unique(score_list, return_counts=True)).T
-	#rolling mean, window of 50 to smooth discrete peaks in low probability Cs
-	scores_w50 = uniform_filter1d(score_count[:,1], size=50)
+
+	xvals = np.linspace(0, 1, 50)
+	scores = np.interp(xvals, score_count[:,0], score_count[:,1])
+	scores = uniform_filter1d(scores, size=3)
+	scores = list(scores/np.sum(scores))
+	scores[0]=0 ; scores[-1]=0
 
 	##Build Plot
 	#BarPlot scores on X and the number of motifs having obtained that score on Y
-	plt.bar(score_count[:,0], scores_w50, align='center', width=0.025)
+	plt.plot(xvals, scores)
+	plt.fill_between(xvals,scores, alpha=0.2)
 
-
-	plt.axvline(x=float(k), color='r', linestyle='--',label=f'Threshold={float(k)}')
-	plt.legend(loc='upper left',fontsize='small')
-
-	plt.ylabel(f'{motif} Site Count')
-	plt.xlabel('Methylation probability')
-	plt.title(f'{lib} {motif}')
+	plt.axvline(x=float(k), color='r', linestyle=(0, (3, 10, 1, 10)),label=f'Threshold={float(k)}')
 	
-	plt.savefig(f'{lib}_{motif}_{k}methyl_prob_plot.pdf')
+	plt.legend(loc='upper left',fontsize='small')
+	plt.ylabel(f'Fraction of {motif} Sites')
+	plt.xlabel('Methylation probability')
+	plt.title(f'{lib} dSMF')
+	
+	plt.savefig(f'{lib}_{k}_methyl_distribution.pdf')
 
 	plt.close()
 
 	return 0
-
-
-########################################################################
-##############################Build BigWig##############################
-########################################################################
-
-strands = [0, 1]
-score_list = []
-##Retrieve the whole score list (pos,score) from the DBs
-for motif in motifs:
-	for strand in strands:
-		score_list = score_list + get_score_list_per_motif(f'./{lib}.{motif}_1/per_read_modified_base_calls.db',strand,motif)
-
-#Store in Numpy array, transform the scores to get 1-fraction methylated
-unlog_sc = np.array(score_list,dtype=np.float64)
-unlog_sc[:,1] = 1 - np.exp(unlog_sc[:,1])
-
-#Apply threshold, all values above threshold will be 1s, under will be 0s
-unlog_sc[:,1] = unlog_sc[:,1] > (1 - float(k))
-
-#Length of CE chromosomes in order (chrI chrII chrIII chrIV chrM chrV chrX)
-chrm_lens = [0,15072434,15279421,13783801,17493829,13794,20924180,17718942]
-#Cumulative length
-cumsum_chrm_lens = np.cumsum(chrm_lens)
-
-#Retrieve data for each chromosome
-for cl in range(1,len(chrm_lens)):
-
-	#Select chromosome-specific scores
-	unlog_sc_perChr = unlog_sc[ (unlog_sc[:,0]>cumsum_chrm_lens[cl-1]) & (unlog_sc[:,0]<=cumsum_chrm_lens[cl]) ]
-	unlog_sc_perChr[:,0] = unlog_sc_perChr[:,0] - cumsum_chrm_lens[cl-1]
-
-	#Aggregate per position with average
-	unlog_sc_unq = pd.DataFrame(unlog_sc_perChr).groupby(0).mean()
-	#Apply a 10-base rolling window average
-	unlog_sc_unq_rwa = unlog_sc_unq.rolling(10,min_periods=1).mean()
-	#Output to txt
-	unlog_sc_unq.to_csv(path_or_buf=f'{cl}.txt',sep=' ', header=False)
-	unlog_sc_unq_rwa.to_csv(path_or_buf=f'{cl}_w10.txt',sep=' ', header=False)
-
-########################################################################
-##########################Build Methyl Plot#############################
-########################################################################
-
-#save_methyl_prob_plot(get_scores(f'./{lib}.HCG_1/per_read_modified_base_calls.db'),'CpG',lib,k)
-
-#save_methyl_prob_plot(get_scores(f'./{lib}.GCH_1/per_read_modified_base_calls.db'),'GpC',lib,k)
-
-save_methyl_prob_plot(get_scores(f'./per_read_modified_base_calls.db'),'CpG_GpC',lib,k)
 	
 
+def main():
+
+	########################################################################
+	##############################Build BigWig##############################
+	########################################################################
+
+	strands = [0, 1]
+	score_list = []
+	##Retrieve the whole score list (pos,score) from the DBs
+	for motif in motifs:
+		for strand in strands:
+			score_list = score_list + get_score_list_per_motif(f'./{lib}.{motif}_1/per_read_modified_base_calls.db',strand,motif)
+
+	#Store in Numpy array
+	log_sc = np.array(score_list,dtype=np.float64)
+	#Transform the scores to get 1-fraction methylated
+	unlog_sc = np.copy(log_sc)
+	unlog_sc[:,1] = 1 - np.exp(unlog_sc[:,1])
+
+	#Apply threshold, all values above threshold will be 1s, under will be 0s
+	unlog_sc[:,1] = unlog_sc[:,1] > (1 - float(k))
+
+	#Cumulative length
+	cumsum_chrm_lens = np.cumsum(chrm_lens)
+
+	#Retrieve data for each chromosome
+	for cl in range(1,len(chrm_lens)):
+
+		#Select chromosome-specific scores
+		unlog_sc_perChr = unlog_sc[ (unlog_sc[:,0]>cumsum_chrm_lens[cl-1]) & (unlog_sc[:,0]<=cumsum_chrm_lens[cl]) ]
+		unlog_sc_perChr[:,0] = unlog_sc_perChr[:,0] - cumsum_chrm_lens[cl-1]
+
+		#Aggregate per position with average
+		unlog_sc_unq = pd.DataFrame(unlog_sc_perChr).groupby(0).mean()
+		#Apply a 10-base rolling window average
+		unlog_sc_unq_rwa = unlog_sc_unq.rolling(10,min_periods=1).mean()
+		#Output to txt
+		unlog_sc_unq.to_csv(path_or_buf=f'{cl}.txt',sep=' ', header=False)
+		unlog_sc_unq_rwa.to_csv(path_or_buf=f'{cl}_w10.txt',sep=' ', header=False)
+
+	########################################################################
+	##########################Build Methyl Plot#############################
+	########################################################################
+
+	save_methyl_prob_plot(np.exp(log_sc),'CpG_GpC',lib,k)
+
+if __name__ == "__main__":
+	main()
