4406c43f598e856af16c016b365b7ee1080168b8
AAnnan <ahrmad.annan@students.unibe.ch>
2020-10-02 18:37:20 +0200

    fix dependency issues

--- Mega_wrapper.sh
+++ Mega_wrapper.sh
@@ -29,11 +29,14 @@ echo -e "Megalodon Outputs:" '\t' $outputs
 if [ "${two_pass}" = "yes" ]; then
 	echo "2-pass demultiplexing: (1) Barcoding with DeepBinner..."
 	step1=$(sbatch 01a_Demux_DeepBinner.sh)
+	step1="${step1//[^0-9]/}"
 	echo "2-pass demultiplexing: (2) Barcoding with Guppy Barcoder..."
 	step2=$(sbatch --dependency=afterany:$step1 01b_Demux_Guppy.sh)
+	step2="${step2//[^0-9]/}"
 elif [ "${two_pass}" = "no" ]; then
 	echo "1-pass demultiplexing: Barcoding with Guppy Barcoder..."
 	step2=$(sbatch 01b_Demux_Guppy.sh)
+	step2="${step2//[^0-9]/}"
 else
 	echo "Invalid two_pass variable in varSettings.sh. Must be yes or no."
     exit
@@ -42,6 +45,7 @@ fi
 ### MEGALODON
 echo "Megalodon augmented BaseCalling..."
 step3=$(sbatch --dependency=afterany:$step2 02_Megalodon.sh)
+step3="${step3//[^0-9]/}"
 
 ### ANALYSIS
 echo "Building dSMF BigWig and Methylation plot..."
